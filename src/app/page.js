73e98400cbd49551c0096d78736f1b612e64e1a7
Lioncards <159770992+Lioncards@users.noreply.github.com>
2024-05-14 20:43:57 +0100

    awed

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,13 @@
 
 import React from "react"
 import Image from "next/image"
+import HomeSectionOne from "@/Components/HomeSection/HomeSectionOne"
 
 const page=()=>{
   
   return(
     <div>
-        <p>Home</p>
+        <HomeSectionOne/>
     </div>
   )
 }
