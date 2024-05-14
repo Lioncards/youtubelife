7477ce022877c4253d74cee9f31bee628aa38efa
Lioncards <159770992+Lioncards@users.noreply.github.com>
2024-05-14 23:31:47 +0100

    ased

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,31 +3,33 @@
 import React from "react"
 import Image from "next/image"
 import { useState } from "react"
-import { FaBars } from "react-icons/fa";
+import { HiMiniBars3 } from "react-icons/hi2";
 import image1 from '@/Components/Images/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.webp'
 import { FaMicrophone } from "react-icons/fa";
 import { RiVideoAddLine } from "react-icons/ri";
 import { FaRegBell } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
 
 const Navbar=()=>{
 
     return(
         <div>
-            <div>
-                <div>
-                    <FaBars />
-                    <div>
-                        <Image src={image1} className="w-20"/>
-                        <p>Youtube</p>
+            <div className="flex items-center px-7">
+                <div className="flex items-center w-full gap-7">
+                    <HiMiniBars3  className="text-xl"/>
+                    <div className="flex items-center">
+                        <Image src={image1} className="w-12"/>
+                        <p>YouTube</p>
                     </div>
                 </div>
 
-                <div>
-                    <input/>
+                <div className="flex w-full items-center justify-center">
+                    <input className="px-36 py-2"/>
+                    <IoSearchOutline className="text-3xl bg-gray-500" />
                     <FaMicrophone />
                 </div>
 
-                <div>
+                <div className="flex w-full items-center justify-end">
                     <RiVideoAddLine />
                     <FaRegBell />
                     <p>O</p>
